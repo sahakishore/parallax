348b35ed62442a8c36e80e1867788d0a7467e84b
Kishore Saha <sahakishore4@gmail.com>
2021-09-02 19:08:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+// import './App.css';
 
-function App() {
+
+import axios from 'axios'
+
+import {Table} from 'react-bootstrap'
+
+//  const baseURL = "https://data.covid19india.org/state_district_wise.json"
+ const baseURL = "https://cors-anywhere.herokuapp.com/https://api.covid19india.org/data.json"
+//  const baseURL = "https://jsonplaceholder.typicode.com/posts/1"
+
+const App = () => {
+  
+  const [receiveData, setReceiveData] = useState([])
+  const [errors, setErrors] = useState(null)
+
+    const networkCall = async () => {
+      try {
+        const response = await axios.get(baseURL)
+        // const response = await fetch(baseURL)
+        console.log("Full response:", response)
+        const { data, errors } = response
+
+         setReceiveData(data.statewise)
+        
+        // console.log("All States:",Object.entries(data))
+        
+        
+        if (errors) setErrors(errors)
+      } catch (error) {
+        setErrors([error])
+      }
+    }
+
+
+  useEffect(() => {
+  
+    networkCall()
+
+  },[])
+
+  console.log("received Data:",receiveData)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container mt-5">
+      <div className="main-heading">
+      <h2 className="mb-5"><span className="font-weight-bold">Covid19 Statewise Stats in India</span></h2>
+      </div>
+     
+     <div>
+     {
+        <Table  striped bordered hover responsive >
+          <thead className="thead-dark th">
+            <th>STATE</th>
+            <th>CONFIRMED</th>
+            <th>RECOVERED</th>
+            <th>DEATHS</th>
+            <th>ACTIVE</th>
+          </thead>
+          <tbody>
+            {
+              receiveData.map((items, key) => {
+                return (
+                <tr key={key}>
+                  <td>{items.state}</td>
+                  <td>{items.confirmed}</td>
+                  <td>{items.recovered}</td>
+                  <td>{items.deaths}</td>
+                  <td>{items.active}</td>
+                </tr>
+                )
+              })
+            }
+          </tbody>
+        </Table>
+     }
+       
+     </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
